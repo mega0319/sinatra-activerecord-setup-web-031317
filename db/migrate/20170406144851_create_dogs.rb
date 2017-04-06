e47053eb151525921f181bb3b37ca8973ffc22d7
Naz Khan <nazmuskhan@yahoo.com>
2017-04-06 10:51:34 -0400

    Done.

---
+++ db/migrate/20170406144851_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
+
+  end
+end
